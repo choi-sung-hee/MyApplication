fcb4f6f153a70222395cdcfcba4f03ac5822d550
pcuser89 <as01027568273@gmail.com>
2021-12-30 16:05:47 +0900

    First Commit

--- app/src/main/java/com/example/myapplication/register.kt
+++ app/src/main/java/com/example/myapplication/register.kt
@@ -1,11 +1,11 @@
-package com.example.myapplication;
+package com.example.myapplication
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 
- class MainActivity : AppCompatActivity() {
+class register : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_register)
     }
 }
---
+++ app/src/main/res/layout/MainActivity/java.kt
@@ -0,0 +1,4 @@
+package layout.MainActivity
+
+class java {
+}
